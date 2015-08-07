263372dc91c79801e47068685bea98c5c8152780
Nathaniel Clapp <github@nattyclapp.com>
2015-08-07 16:25:41 -0500

    add #draw_board method

--- lib/game.rb
+++ lib/game.rb
@@ -17,28 +17,28 @@ class Game
 
   def check_for_unique_gamepiece
     while @player_one == @player_two
-      puts "#{@player_two} is in use, please pick another character:"
+      puts "\"#{@player_two}\" is in use, please pick another character:"
       @player_two = gets.chomp.to_s
     end
   end
 
-  def start_game
-    puts "Welcome to my Tic Tac Toe game"
+  def draw_board
     puts "|_#{@board[0]}_|_#{@board[1]}_|_#{@board[2]}_|
         \n|_#{@board[3]}_|_#{@board[4]}_|_#{@board[5]}_|
-        \n|_#{@board[6]}_|_#{@board[7]}_|_#{@board[8]}_|
-        \n"
+        \n|_#{@board[6]}_|_#{@board[7]}_|_#{@board[8]}_|"
+  end
+
+  def start_game
+    puts "Welcome to my Tic Tac Toe game"
     prompt_for_gamepiece
+    draw_board
     puts "Please select your spot."
     until game_is_over(@board) || tie(@board)
       get_human_spot
       if !game_is_over(@board) && !tie(@board)
         eval_board
       end
-      puts "|_#{@board[0]}_|_#{@board[1]}_|_#{@board[2]}_|
-          \n|_#{@board[3]}_|_#{@board[4]}_|_#{@board[5]}_|
-          \n|_#{@board[6]}_|_#{@board[7]}_|_#{@board[8]}_|
-          \n"
+      draw_board
     end
     puts "Game over"
   end
@@ -123,5 +123,4 @@ class Game
 end#class Game
 
 game = Game.new
-# game.start_game
-game.prompt_for_gamepiece
+game.start_game
