68573d42622a67b9b81212eeb390852c5882f813
Nathaniel Clapp <github@nattyclapp.com>
2015-08-07 15:30:48 -0500

    quick once over, fix spacing add Class end comment

--- lib/game.rb
+++ lib/game.rb
@@ -82,15 +82,14 @@ class Game
   end
 
   def game_is_over(b)
-
     [b[0], b[1], b[2]].uniq.length == 1 ||
-    [b[3], b[4], b[5]].uniq.length == 1 ||
-    [b[6], b[7], b[8]].uniq.length == 1 ||
-    [b[0], b[3], b[6]].uniq.length == 1 ||
-    [b[1], b[4], b[7]].uniq.length == 1 ||
-    [b[2], b[5], b[8]].uniq.length == 1 ||
-    [b[0], b[4], b[8]].uniq.length == 1 ||
-    [b[2], b[4], b[6]].uniq.length == 1
+      [b[3], b[4], b[5]].uniq.length == 1 ||
+      [b[6], b[7], b[8]].uniq.length == 1 ||
+      [b[0], b[3], b[6]].uniq.length == 1 ||
+      [b[1], b[4], b[7]].uniq.length == 1 ||
+      [b[2], b[5], b[8]].uniq.length == 1 ||
+      [b[0], b[4], b[8]].uniq.length == 1 ||
+      [b[2], b[4], b[6]].uniq.length == 1
   end
 
   def tie(b)
